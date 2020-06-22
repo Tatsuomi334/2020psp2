b41ce4bcc8cc8ed1bc2109aa8afe045c1c35c531
Tatsuhiro <bb35319029@ms.nagasaki-u.ac.jp>
2020-06-22 15:14:01 +0900

    課題２

--- k02/k02.c
+++ k02/k02.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
-extern double p_stdnorm(double z);
+double p_stdnorm(double z)
+{
+return  exp(-z*z/2)/sqrt(2*M_PI);
+}
 
 
 int main(void)
 {
+    double z;
     double val;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
     double L1=1,L2=1;
     double max_val, min_val;
+    double a;
+    double L_A, L_B,mu_a,var_a,mu_b,var_b,L1,L2;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -28,11 +35,10 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
-
+        z=(val-mu_a)/ var_a;
+        L1=p_stdnorm(z)*L1;
+        z=(val-mu_b)/var_b;
+        L2=p_stdnorm(z)*L2;
 
     }
 
@@ -41,16 +47,14 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("L_A: %f\n",max_val);
-    printf("L_B: %f\n",min_val);
+    printf("L_A: %f\n",L1);
+    printf("L_B: %f\n",L2);
 
     return 0;
 
 
 }
 
-double p_stdnorm(double z)
-{
-    return 1/sqrt(2*M_PI) * exp(-z*z/2);
-}
+
+
 
