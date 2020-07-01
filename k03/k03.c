cf8283d573352c09824d3a93c3a232102ce45eb5
Tatsuhiro <bb35319029@ms.nagasaki-u.ac.jp>
2020-07-01 15:12:54 +0900

    課題３提出

--- k03/k03.c
+++ k03/k03.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #ifdef CONST_SEED
 #define RAND_SEED 2020
@@ -9,36 +10,6 @@
 #define RAND_SEED ((unsigned) time(NULL))
 #endif
 
-extern double r_unif(void);
-extern double r_stdnorm(void);
-
-int main(int argc, char* argv[])
-{
-    int num_dummy;
-    double mu;
-    double sigma;
-    int i;
-    double dummy;
-
-
-
-
-
-    printf("============================================\n");
-    printf("template mean: %4.1lf\n",mu);
-    printf("template standard deviation: %3.2lf\n",sigma);
-    printf("Num of dummy data: %d\n",num_dummy);
-    printf("============================================\n");
-
-    srand(RAND_SEED);
-    for(  ){
-        /* r_stdnormを使って，1人のデータを捏造 */
-        dummy =   ;
-        printf("%5.2lf\n",dummy);
-    }
-
-    return EXIT_SUCCESS;
-}
 
 double r_unif(void)
 {
@@ -49,3 +20,46 @@ double r_stdnorm(void)
 {
     return sqrt( -2.0*log(r_unif()) ) * sin( 2.0*M_PI*r_unif() );
 }
+
+
+
+
+int main(int argc, char* argv[])
+{
+    int num_dummy;
+    double mu;
+    double sigma;
+    int i;
+    double dummy;
+    double a,b;
+    char fname[FILENAME_MAX];
+    char buf[256];
+    FILE* fp;
+     
+
+
+
+    sscanf(argv[argc-3],"%lf",&mu);
+    sscanf(argv[argc-2],"%lf",&sigma);
+    sscanf(argv[argc-1],"%d",&num_dummy);
+
+    printf("============================================\n");
+    printf("template mean: %3.2lf\n",mu);
+    printf("template standard deviation: %3.2lf\n",sigma);
+    printf("Num of dummy data: %d\n",num_dummy);
+    printf("============================================\n");
+
+    srand(RAND_SEED);
+   
+    
+      for(i=1;i<=num_dummy;i++){
+        /* r_stdnormを使って，1人のデータを捏造 */
+        
+
+        dummy = r_stdnorm()+mu  ;
+        printf("%5.2lf\n",dummy);
+    
+      }
+    return EXIT_SUCCESS;
+}
+
--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    double val;
+    char fname[FILENAME_MAX];
+    char buf[256];
+    char ID[14]
+    char 
+FILE* fp;
+
+    printf("input the filename of sample height:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("Input the filename of sampl3 ID: %s\n",fname);
+    fgets(fname,sizeof(fname),stdin);
+    printf("Which ID's date do you what?:");
+    fgets(fname,sizeof(fname),stdin);
+
+    fp = fopen(fname,"r");
+    if(fp==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
 
